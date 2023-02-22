d1b682ccf9331a57e2a42c075a100e8c4c27d615
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-23 01:37:30 +0330

    add ser id

--- evaluation_score/serializers.py
+++ evaluation_score/serializers.py
@@ -1,9 +1,19 @@
 from .models import *
 from rest_framework import serializers
 from authentication.serializers import UserDataEvaluateResultSerializer
+from authentication.models import Account
 
 
 class UserEvaluationScoreSerializer(serializers.ModelSerializer):
+
+    user = UserDataEvaluateResultSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=Account.objects.all(),
+        source='user'
+    )
+
+    
+
     class Meta:
         model = UserEvaluationScore
         fields = [

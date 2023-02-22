76e36efda048869fa04b99e5c3c48d3535dcbc4f
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-23 01:25:34 +0330

    add ser id

--- evaluation_score/views.py
+++ evaluation_score/views.py
@@ -4,7 +4,7 @@ from .serializers import *
 from rest_framework import viewsets
 
 # Create your views here.
-class UserEvaluationScoreViewSet(viewsets.ReadOnlyModelViewSet):
+class UserEvaluationScoreViewSet(viewsets.ModelViewSet):
     serializer_class = UserEvaluationScoreSerializer
     queryset = UserEvaluationScore.objects.all()
     filterset_fields = ['user', 'metric', 'comment', 'rating', 'compare', 'questionnaire']

622fa7748cbcbca8cb7d1322dcca8e9ece3b4dc4
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-25 00:08:05 +0330

    add Notifications

--- notification/views.py
+++ notification/views.py
@@ -7,8 +7,8 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = NotificationSerializer
     queryset = Notification.objects.all()
 
-    def list(self, request, *args, **kwargs):
-        print(request.user)
-        self.queryset = Notification.objects.get(user=request.user.id)[0:20]
-        serializer = self.get_serializer(self.queryset, many=True)
-        return Response(serializer.data)
+    # def list(self, request, *args, **kwargs):
+    #     print(request.user)
+    #     self.queryset = Notification.objects.get(user=request.user.id)[0:20]
+    #     serializer = self.get_serializer(self.queryset, many=True)
+    #     return Response(serializer.data)

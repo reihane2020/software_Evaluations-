7432076cb3b550839e0fe7646d1bc8b0270756c8
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-25 18:45:48 +0330

    add reply comment

--- comment_reply/views.py
+++ comment_reply/views.py
@@ -2,22 +2,21 @@ from .models import *
 from .serializers import *
 from rest_framework import viewsets
 from rest_framework.response import Response
-from comment_evaluation.models import CommentEvaluate
+from comment_evaluation.models import CommentEvaluateResult
+from comment_evaluation.serializers import CommentEvaluateForResultSerializer
+
+
 
 # Create your views here.
-class CommentReplyViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = CommentResultSerializer
-    queryset = CommentEvaluate.objects.filter(
-        publish=True,
-    )
-    filterset_fields = ['software']
+class CommentReplyViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentEvaluateForResultSerializer
+    queryset = CommentEvaluateResult.objects.all()
 
 
     def list(self, request, *args, **kwargs):
+        print(request.data["pid"])
         queryset = self.filter_queryset(
-            CommentEvaluate.objects.filter(
-                publish=True,
-            )
+            CommentEvaluateResult.objects.filter(evaluate=request.data["pid"])
         )
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)

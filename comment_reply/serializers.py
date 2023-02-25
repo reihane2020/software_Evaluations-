5052823f13545778115358631c3c25bc4b827b60
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-25 13:58:05 +0330

    fix usercomment reply

--- comment_reply/serializers.py
+++ comment_reply/serializers.py
@@ -1,6 +1,7 @@
 from .models import *
 from rest_framework import serializers
-
+from comment_evaluation.models import CommentEvaluate
+from comment_evaluation.serializers import CommentEvaluateForResultSerializer
 
 class CommentReplySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,23 @@ class CommentReplySerializer(serializers.ModelSerializer):
             'content',
             'datetime'
         ]
+
+
+class CommentResultSerializer(serializers.ModelSerializer):
+
+    data = serializers.SerializerMethodField("byList")
+    
+    def byList(self, obj):
+        cc = CommentEvaluateResult.objects.filter(evaluate=obj.pk)
+        ss = CommentEvaluateForResultSerializer(cc, many=True)
+        return ss.data
+
+    class Meta:
+        model = CommentEvaluate
+        fields = [
+            'id',
+            'section',
+            'evaluates',
+            'data'
+        ]
+        depth = 1
--- comment_reply/views.py
+++ comment_reply/views.py
@@ -2,10 +2,24 @@ from .models import *
 from .serializers import *
 from rest_framework import viewsets
 from rest_framework.response import Response
-
-
+from comment_evaluation.models import CommentEvaluate
 
 # Create your views here.
 class CommentReplyViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = CommentReplySerializer
-    queryset = CommentReply.objects.all()
+    serializer_class = CommentResultSerializer
+    queryset = CommentEvaluate.objects.filter(
+        publish=True,
+    )
+    filterset_fields = ['software']
+
+
+    # def list(self, request, *args, **kwargs):
+    #     queryset = self.filter_queryset(
+    #         Notification.objects.filter(
+    #             user=self.request.user,
+    #         ).order_by("-id")[0:20]
+    #     )
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+

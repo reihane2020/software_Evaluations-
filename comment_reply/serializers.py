b4fe3f89e7784118cb1a9858761a51f706a4501f
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-25 22:48:23 +0330

    fix comment insert

--- comment_reply/serializers.py
+++ comment_reply/serializers.py
@@ -2,14 +2,21 @@ from .models import *
 from rest_framework import serializers
 from comment_evaluation.models import CommentEvaluate, CommentEvaluateResult
 from authentication.serializers import UserDataEvaluateResultSerializer
-
+from authentication.models import Account
 
 
 class CommentReplySerializer(serializers.ModelSerializer):
+    user = UserDataEvaluateResultSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=Account.objects.all(),
+        source='user'
+    )
+
     class Meta:
         model = CommentReply
         fields = [
             'user',
+            'user_id',
             'parent',
             'content',
             'datetime'
--- comment_reply/views.py
+++ comment_reply/views.py
@@ -12,13 +12,16 @@ class CommentReplyViewSet(viewsets.ModelViewSet):
     serializer_class = CommentEvaluateForReplySerializer
     queryset = CommentEvaluateResult.objects.all()
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def create(self, request, *args, **kwargs):
+        serializer = CommentReplySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
     def list(self, request, *args, **kwargs):
         _pid = self.request.GET.get('pid', None)
-        print(_pid)
         queryset = self.filter_queryset(
             CommentEvaluateResult.objects.filter(evaluate=_pid)
         )

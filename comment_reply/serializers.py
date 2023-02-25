a652f5dee17392a176d59efc7c959c9bc0758062
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-02-25 17:33:11 +0330

    add reply comment

--- comment_reply/serializers.py
+++ comment_reply/serializers.py
@@ -23,10 +23,11 @@ class CommentEvaluateForResultSerializer(serializers.ModelSerializer):
     reply_count = serializers.SerializerMethodField("getreply")
 
     def getreply(self, obj):
-        # cc = CommentReply.objects.filter(parent=obj.pk)
+        cc = CommentReply.objects.filter(parent=obj)
         # ss = CommentReplySerializer(cc, many=True)
         print("obj.pk")
         print(obj)
+        print(cc)
         return obj
 
     class Meta:
